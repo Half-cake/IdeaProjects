d392b6e6820abd28c49122444cf98420ad544916
Half-cake <alekseyzhulanov@gmail.com>
2016-12-02 16:03:42 +0700

    All

--- WindowMy/src/sample/Controller.java
+++ WindowMy/src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
+import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+
 import javafx.collections.ObservableList;
 import javafx.collections.ArrayChangeListener;
 import javafx.collections.FXCollections;
@@ -36,14 +38,22 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {};
 
 
-    public void setAddItem(ActionEvent event){
-        ObservableList<String> data = FXCollections.observableArrayList();
+     public void fullListView() throws Exception {
+         ListView<Object> incomingList = new ListView<>();
+         incomingList = ItemToDO.readFromFile();
 
+     }
+
+    public void setAddItem(ActionEvent event) throws Exception {
+        ObservableList<String> data = FXCollections.observableArrayList();
         String title = stringForAdding.getText();
         String status = setStatusRadioButton.isSelected() ? "Important":"Without";
-        ItemToDO oneItem = new ItemToDO(title, status);
+        Date time = new Date();
+        long timeStamp = time.getTime();
+
+        ItemToDO oneItem = new ItemToDO(title, status, timeStamp);
         //Добавляем в Обсерв лист
-        data.add(oneItem.title);
+        data.add(oneItem.getTitle());
         //Добавляем из ОбсервЛиста в основной лист
         myToDo.getItems().addAll(data);
 
@@ -54,8 +64,9 @@ public class Controller implements Initializable {
     }
     public void setOnMouseClicked(MouseEvent event){
         deleteItem.setOnAction(event1 -> {
-                    myToDo.getItems().remove(myToDo.getFocusModel().getFocusedIndex());
-                }
+            myToDo.getItems().remove(myToDo.getSelectionModel().getSelectedIndex());
+            System.out.println(myToDo.getSelectionModel().getSelectedIndex());}
+
         );
     }
 }
--- WindowMy/src/sample/ItemToDO.java
+++ WindowMy/src/sample/ItemToDO.java
@@ -1,9 +1,16 @@
 package sample;
 
 import com.google.gson.Gson;
+import javafx.scene.control.ListView;
 
+import java.io.FileReader;
+import java.util.Date;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 
 /**
@@ -11,24 +18,25 @@ import java.io.IOException;
  */
 public class ItemToDO {
 
-    String title;
-    String status;
-    String defaultStatus = "not set";
+    private String title;
+    private String status;
+    private long timeStamp;
+    Date createData;
+
 
     public ItemToDO(String title){
         this.title = title;
-        this.status = defaultStatus;
-
     }
 
-    public ItemToDO(String title, String status) {
+    public ItemToDO(String title, String status, long timeStamp) {
         this.title = title;
         this.status = status;
+        this.timeStamp = timeStamp;
     }
 
     public void setTitle(String title) {
         this.title = title;
-    }
+        }
 
     public String getTitle() {
         return title;
@@ -38,11 +46,15 @@ public class ItemToDO {
         this.status = status;
     }
 
-    public static void writeToFile(Object o){
+
+    public static void writeToFile(Object o) throws IOException {
         Gson gson = new Gson();
+        File file = new File("todo.json");
+        if (!file.exists()) file.createNewFile();
+
         try
         {
-            FileWriter writer = new FileWriter("C:\\Users\\Aleksey Zhulanov\\IdeaProjects\\WindowMy\\src\\todo.json", true);
+            FileWriter writer = new FileWriter(file.getAbsolutePath(), true);
             String json = gson.toJson(o);
             writer.write(json+"\n");
             writer.close();
@@ -50,5 +62,22 @@ public class ItemToDO {
             e.printStackTrace();
         }
     }
+    public static ListView readFromFile()throws Exception {
+        //Подключение gson
+        Gson gson = new Gson();
+        //Создаем лист для ипорта
+        ListView<ItemToDO> itemsForImport = new ListView<ItemToDO>();
+        //Находим файл с содержимым
+        FileReader file = new FileReader("todo.json");
+        //Сканер для содержимого
+        Scanner sc = new Scanner(file);
 
+        while (sc.hasNextLine()){
+            String fromFile = sc.nextLine();
+            ItemToDO itemToDO = gson.fromJson(fromFile, ItemToDO.class);
+            itemsForImport.getItems().addAll(itemToDO);
+        }
+
+    return itemsForImport;
+    }
 }
